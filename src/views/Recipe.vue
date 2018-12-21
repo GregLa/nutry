db49a99e38eff79b96d1f2bb0fc38d3c7cc72791
Grégoire Lagoutte <gregoire.lagoutte@itycom.com>
2018-12-21 22:44:47 +0100

    Recipe add ingredients and update pattern

--- src/views/Recipe.vue
+++ src/views/Recipe.vue
@@ -7,6 +7,7 @@
           <v-text-field
             label="Nom"
             v-model="recipe.name"
+            @change="update()"
           ></v-text-field>
         </v-flex>
         <v-flex xs12 sm6 pa-3>
@@ -14,6 +15,7 @@
             label="Nb personnes"
             type="number"
             v-model.number="recipe.persons"
+            @change="update()"
           ></v-text-field>
         </v-flex>
       </v-layout>
@@ -38,21 +40,25 @@
               :items="ingredients"
               item-text="name"
               item-value="id"
-              disabled
             ></v-autocomplete>
           </v-flex>
           <v-flex xs12 sm6 pl-3>
             <v-text-field
               label="Quantité (g)"
-              v-model="ingredient.grams"
-              @change="$forceUpdate()"
+              type="number"
+              v-model.number="ingredient.grams"
+              @change="update()"
             />
           </v-flex>
         </v-layout>
       </v-layout>
+      <v-divider></v-divider>
+      <v-card-actions>
+        <v-btn flat color="info" @click="addIngredient">ajouter ingrédients</v-btn>
+      </v-card-actions>
     </v-card>
 
-    <v-card>
+    <v-card class="mb-3">
       <v-list dense>
         <v-list-tile>
           <v-list-tile-content><h2>Informations nutritionnelles</h2></v-list-tile-content>
@@ -99,6 +105,17 @@
             <v-list-tile-content class="align-end">{{ getSalt }}</v-list-tile-content>
           </v-list-tile>
         </v-list>
+    </v-card>
+
+    <v-card>
+      <v-layout justify-end v-if="creationMode">
+        <v-btn color="success" :disabled="!canSave">Enregrister</v-btn>
+      </v-layout>
+      <v-layout justify-space-between v-else>
+        <v-btn color="info" :disabled="!canUpdate">Mettre à jour</v-btn>
+        <v-btn color="warning" :disabled="!dataModified">Annuler</v-btn>
+        <v-btn color="error" >Supprimer</v-btn>
+      </v-layout>
     </v-card>
 
   </v-container>
@@ -157,21 +174,45 @@ export default {
     getSalt () {
       const totalSalt = this.getNutritionnalTotalByValueType('salt')
       return this.getNutrionValueByScale(totalSalt).toFixed(2)
+    },
+
+    canSave () {
+      return !!this.recipe.name && !!this.recipe.persons
+    },
+
+    canUpdate () {
+      return !!this.recipe.name && !!this.recipe.persons && this.dataModified
+    },
+
+    dataModified: {
+      set (val) {
+        this.updated = val
+      },
+      get (val) {
+        return this.updated
+      }
     }
+
   },
 
   data: () => ({
-    recipe: {},
-    scale: 'total',
-    scales: [{ text: '/ 100g', value: '100g' }, { text: '/ personne', value: 'persons' }, { text: 'Total', value: 'total' }]
+    creationMode: false,
+    updated: false,
+    recipe: {
+      name: '',
+      persons: 1,
+      composition: [{ ingredientId: null, grams: null }]
+    },
+    ingredient: { ingredientId: null, grams: null },
+    scale: 'persons',
+    scales: [{ text: '/ 100g', value: '100' }, { text: '/ personne', value: 'persons' }, { text: 'Total', value: 'total' }]
   }),
 
   // Navigation guard
   async beforeRouteEnter (to, from, next) {
     const id = to.params.id
     if (id === 'new') {
-      const recipe = await store.getters['recipes/getRecipeModel']
-      next(vm => vm.setRecipe(recipe))
+      next(vm => { vm.creationMode = true })
     } else if (!isNaN(Number(id))) {
       const recipe = await store.getters['recipes/getRecipe'](Number(id))
       if (recipe) {
@@ -187,6 +228,16 @@ export default {
   methods: {
     setRecipe (recipe) {
       this.recipe = recipe
+    },
+
+    update () {
+      this.$forceUpdate()
+      this.dataModified = true
+    },
+
+    addIngredient () {
+      const ingredientModel = deepCopy(this.ingredient)
+      this.recipe.composition.push(ingredientModel)
     },
 
     getNutritionnalTotalByValueType (type) {
@@ -206,7 +257,7 @@ export default {
 
     getNutrionValueByScale (value) {
       switch (this.scale) {
-        case '100g':
+        case '100':
           return (100 * value) / this.totalWeight
         case 'persons':
           return value / this.recipe.persons
@@ -214,10 +265,6 @@ export default {
           return value
       }
     }
-  },
-
-  mounted () {
-
   }
 }
 </script>
